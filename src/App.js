cb0d675ff7e9dbf420c1450f9f49d40c1f69bae5
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-07-20 16:32:58 +0200

    Añadiendo sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import Prueba from './components/Prueba/Prueba';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< Updated upstream
         <h1>Proyecto de React</h1>
         
+=======
+>>>>>>> Stashed changes
       </header>
+      <Prueba></Prueba>
     </div>
   );
 }
---
+++ src/components/Prueba/Prueba.jsx
@@ -0,0 +1,9 @@
+import './Prueba.scss';
+
+const Prueba = () => {
+    return (
+        <h1>Probando</h1>
+    )
+}
+
+export default Prueba;
