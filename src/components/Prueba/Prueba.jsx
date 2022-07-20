639b8cc4fd86f092ccb4ae1a42b0cc6f63a9be44
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-07-20 20:59:47 +0200

    Añadiendo estilos generales

--- src/components/Prueba/Prueba.jsx
+++ src/components/Prueba/Prueba.jsx
@@ -2,45 +2,32 @@ import './Prueba.scss';
 
 const Prueba = () => {
     return (
-        <div className='main-container center'>
-            <div className='col-2'>
-                <div className='col'>
-                    <figure>
-                        <img src='//cdn.shopify.com/s/files/1/0565/9757/7916/collections/Grop_Shot_25335_1_500x600_crop_center.jpg?v=1632994722' alt="Tarta"></img>
+        <div className='products'>
+            <div className='main-container center'>
+                <h2>Sampler box</h2>
+                <a href='' title='Ver más' className='products__link'>Ver más</a>
+                <div className='column-container'>
+                    <div className='col'>
+                        <figure>
+                            <img src='//cdn.shopify.com/s/files/1/0565/9757/7916/collections/Grop_Shot_25335_1_500x600_crop_center.jpg?v=1632994722' alt="Tarta"></img>
+                        </figure>
                         <h4>Tarta de chocolate</h4>
-                    </figure>
-                </div>
-                <div className='col'>
-                    <figure>
-                        <img src='//cdn.shopify.com/s/files/1/0565/9757/7916/collections/Grop_Shot_25335_1_500x600_crop_center.jpg?v=1632994722' alt="Tarta"></img>
+                    </div>
+                    <div className='col'>
+                        <figure>
+                            <img src='//cdn.shopify.com/s/files/1/0565/9757/7916/collections/Grop_Shot_25335_1_500x600_crop_center.jpg?v=1632994722' alt="Tarta"></img>
+                        </figure>
                         <h4>Tarta de chocolate</h4>
-                    </figure>
-                    <h4>Tarta de chocolate</h4>
-                </div>
-            </div>
-            <div className='col-3'>
-            <div className='col'>
-                    <figure>
-                        <img src='//cdn.shopify.com/s/files/1/0565/9757/7916/collections/Grop_Shot_25335_1_500x600_crop_center.jpg?v=1632994722' alt="Tarta"></img>
+                    </div>
+                    <div className='col'>
+                        <figure>
+                            <img src='//cdn.shopify.com/s/files/1/0565/9757/7916/collections/Grop_Shot_25335_1_500x600_crop_center.jpg?v=1632994722' alt="Tarta"></img>
+                        </figure>
                         <h4>Tarta de chocolate</h4>
-                    </figure>
-                    <h4>Tarta de chocolate</h4>
-                </div>
-                <div className='col'>
-                    <figure>
-                        <img src='//cdn.shopify.com/s/files/1/0565/9757/7916/collections/Grop_Shot_25335_1_500x600_crop_center.jpg?v=1632994722' alt="Tarta"></img>
-                        <h4>Tarta de chocolate</h4>
-                    </figure>
-                    <h4>Tarta de chocolate</h4>
-                </div>
-                <div className='col'>
-                    <figure>
-                        <img src='//cdn.shopify.com/s/files/1/0565/9757/7916/collections/Grop_Shot_25335_1_500x600_crop_center.jpg?v=1632994722' alt="Tarta"></img>
-                        <h4>Tarta de chocolate</h4>
-                    </figure>
-                    <h4>Tarta de chocolate</h4>
+                    </div>
                 </div>
             </div>
+            
         </div>
         
     )
