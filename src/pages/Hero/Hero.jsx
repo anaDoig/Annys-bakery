107cb8ba16c3bfeb969853294d99de4a24f04638
Anny Pereira <annypereiras@gmail.com>
2022-07-25 21:59:41 +0200

    act footer/hero/about

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,31 +1,36 @@
 import "./Hero.scss";
+import { useState } from "react";
 import backImg from "../../assets/img/background.jpg";
-import cupcakeImg from "../../assets/img/lemonCupcakes.jpg";
+import cupcakeImg from "../../assets/img/cupcakes.jpg";
 import cookieImg from "../../assets/img/redvelvetCookies.jpg";
-import cakeImg from "../../assets/img/tropicalCake1.jpg";
+import cakeImg from "../../assets/img/chocoCake.jpg";
 
 const Hero = () => {
+  const [current, setCurrent] = useState("");
+
   return (
     <>
-      <h1>Inicio</h1>
       <div className="carrusel-container">
-        <div className="slider-container">
-          <div id="slider1" className="slider">
+        <div className="arrow-left" />
+        <div className="arrow-rigth" />
+
+        <div className="slides">
+          <div>
             <img src={backImg} alt="Carrusel" className="img-slider" />
           </div>
-          <div id="slider2" className="slider">
+          <div>
             <img src={cupcakeImg} alt="Carrusel" className="img-slider" />
           </div>
-          <div id="slider3" className="slider">
+          <div>
             <img src={cookieImg} alt="Carrusel" className="img-slider" />
           </div>
-          <div id="slider4" className="slider">
+          <div>
             <img src={cakeImg} alt="Carrusel" className="img-slider" />
           </div>
         </div>
 
-        <div>
-          <button className="button-slider-container">SIGUIENTE</button>
+        {/* <div>
+          <button className="button-slider-container" >SIGUIENTE</button>
           <a href={cupcakeImg}>
             <i className="fa-circle"></i>
           </a>
@@ -35,7 +40,7 @@ const Hero = () => {
           <a href={cakeImg}>
             <i className="fa-circle"></i>
           </a>
-        </div>
+        </div> */}
       </div>
     </>
   );
