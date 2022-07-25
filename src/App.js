63696015c0467ddb54e75ad595397a88c8a0c160
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-07-25 20:08:58 +0200

    Cambiando navbar

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import  Contact from './pages/Contact/Contact';
 import  Products  from './pages/Products/Products';
 import  LogIn from './pages/LogIn/LogIn';
 import  ShoppingCart  from './pages/ShoppingCart/ShoppingCart';
-import Prueba from './components/Prueba/Prueba'
-import { Route, Routes } from "react-router-dom"
-import Menu from "./Menu"
+import ProductsComponent from './components/ProductsComponent/ProductsComponent';
+import { Route, Routes } from "react-router-dom";
+import NavBar from './components/NavBar/NavBar';
+import Cookies from './pages/Cookies/Cookies';
+import Cupcakes from './pages/Cupcakes/Cupcakes';
+import Cakes from './pages/Cakes/Cakes';
 
 function App() {
 
   return (
     <>
-      <Menu />
-          <div className="navbar">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/aboutus" element={<AboutUs />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/login" element={<LogIn />} />
-              <Route path="/shoppingcart" element={<ShoppingCart />} />
-            </Routes>
-          </div>
-        <Prueba></Prueba>
+      <NavBar />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/shoppingcart" element={<ShoppingCart />} />
+        <Route path="/cookies" element={<Cookies />} />
+        <Route path="/cakes" element={<Cakes />} />
+        <Route path="/cupcakes" element={<Cupcakes />} /> 
+      </Routes>
     </>
   )
 }
