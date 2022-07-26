0076350657a89e495a1dbb65f520abb010a61466
Denesis Peralta <dcolmenares1901@gmail.com>
2022-07-26 20:29:46 +0200

    shoppingCart

--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -7,6 +7,11 @@ import { useNavigate } from "react-router-dom";
 import { IconTrash } from "../../components/Icon/IconTrash";
 
 const calculateTotal = (items) => items.reduce((total, { price, quantity }) => price * quantity + total, 0);
+const useCounter = () => {
+  const [count, setCount] = useState();
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+};
 
 const ShoppingCart = () => {
   const navigate = useNavigate();
@@ -30,17 +35,21 @@ const ShoppingCart = () => {
 
   return (
     <>
-      <div className='main-container'>
+      <div className='main-container padding-top'>
         <div className='col-2'>
           <div className='col'>
             {items.map(({ name, id, price, img, size, quantity }) => (
               <div key={id}>
-                <div className='container'>
+                <div className='items_container'>
                   <img className='image' src={img} alt='' />
                   <div className='container-option'>
                     <h4 className='title'>{name}</h4>
                     <p className='size'>Tamaño: {size}</p>
                   </div>
+                  {/* <div>
+                    <button onClick={() => btonIncrement.increment()}>+</button>
+                    <button onClick={() => btonDecrement.decrement()}>-</button>
+                  </div> */}
                   <select onChange={(event) => handleUpdate(id, event.target.value)} value={quantity} className='quantity' name='quantity'>
                     {Array.from({ length: 9 }).map((_, index) => (
                       <option key={`option_${index}`} value={index + 1}>
