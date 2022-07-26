726a70ef1ac7f1e4a60f2e7ee7512e38fc7fc993
Denesis Peralta <dcolmenares1901@gmail.com>
2022-07-26 23:33:30 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import './App.css';
-import  Home  from './pages/Home/Home';
-import  AboutUs  from './pages/AboutUs/AboutUs';
-import  Contact from './pages/Contact/Contact';
-import  Products  from './pages/Products/Products';
-import  LogIn from './pages/LogIn/LogIn';
-import  ShoppingCart  from './pages/ShoppingCart/ShoppingCart';
-import ProductsComponent from './components/ProductsComponent/ProductsComponent';
+import "./App.css";
+import Home from "./pages/Home/Home";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Contact from "./pages/Contact/Contact";
+import Products from "./pages/Products/Products";
+import LogIn from "./pages/LogIn/LogIn";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
+import ProductsComponent from "./components/ProductsComponent/ProductsComponent";
 import { Route, Routes } from "react-router-dom";
-import NavBar from './components/NavBar/NavBar';
-import Cookies from './pages/Cookies/Cookies';
-import Cupcakes from './pages/Cupcakes/Cupcakes';
-import Cakes from './pages/Cakes/Cakes';
+import NavBar from "./components/NavBar/NavBar";
+import Cookies from "./pages/Cookies/Cookies";
+import Cupcakes from "./pages/Cupcakes/Cupcakes";
+import Cakes from "./pages/Cakes/Cakes";
 
 function App() {
   return (
     <>
-      <NavBar/>
-          
+      <NavBar />
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/shoppingcart" element={<ShoppingCart />} />
-        <Route path="/cookies" element={<Cookies />} />
-        <Route path="/cakes" element={<Cakes />} />
-        <Route path="/cupcakes" element={<Cupcakes />} /> 
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/aboutus' element={<AboutUs />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/shoppingcart' element={<ShoppingCart />} />
+        <Route path='/cookies' element={<Cookies />} />
+        <Route path='/cakes' element={<Cakes />} />
+        <Route path='/cupcakes' element={<Cupcakes />} />
       </Routes>
     </>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,32 +4,26 @@ import cupcakeImg from "../../assets/img/cupcakes.jpg";
 import cookieImg from "../../assets/img/redvelvetCookies.jpg";
 import cakeImg from "../../assets/img/chocoCake.jpg";
 
-const Home = () => {
- 
-  return (
-    <>
-      <div className="carrusel-container">
-        <div className="arrow-left" />
-        <div className="arrow-rigth" />
+const Home = () => (
+  <div className='carrusel-container'>
+    <div className='arrow-left' />
+    <div className='arrow-rigth' />
 
-        <div className="slides">
-          <div>
-            <img src={backImg} alt="Carrusel" className="img-slider" />
-          </div>
-          <div>
-            <img src={cupcakeImg} alt="Carrusel" className="img-slider" />
-          </div>
-          <div>
-            <img src={cookieImg} alt="Carrusel" className="img-slider" />
-          </div>
-          <div>
-            <img src={cakeImg} alt="Carrusel" className="img-slider" />
-          </div>
-          
-        </div>
+    <div className='slides'>
+      <div>
+        <img src={backImg} alt='Carrusel' className='img-slider' />
       </div>
-    </>
-  );
-};
+      <div>
+        <img src={cupcakeImg} alt='Carrusel' className='img-slider' />
+      </div>
+      <div>
+        <img src={cookieImg} alt='Carrusel' className='img-slider' />
+      </div>
+      <div>
+        <img src={cakeImg} alt='Carrusel' className='img-slider' />
+      </div>
+    </div>
+  </div>
+);
 
 export default Home;
